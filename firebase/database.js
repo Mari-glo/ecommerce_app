f6fdcc2022c33b581ae3d18aa629a382e6279ecc
Mari-glo <marigloria@gmail.com>
2024-06-22 18:21:43 -0400

    subir1

---
+++ firebase/database.js
@@ -0,0 +1,3 @@
+export const apiKey = 'AIzaSyCtqyNjX5lqO8T1IJptW-fHXFqPkoMb0YY'
+export const authUrl = 'https://identitytoolkit.googleapis.com/'
+export const baseUrl = 'https://ecommerce-platform-default-rtdb.firebaseio.com'
